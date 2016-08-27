3cd512970d8c583106e4f2110d3c3e014ed0184f
kamieb03 <kamieb03@outlook.com>
2016-08-26 17:14:43 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,8 +1,10 @@
 # import special libraries already already built in python
 
 # list of options to select from
-possible_activities = []
+possible_activities = ['a','b','c']
 # choice of what we are going to do 
-
+the_activity = possible_activities[1]
 # displays the results to the end user
-print "Possible actiities are:" + str(possible_activities)
+print "Possible actiities are: " + str(possible_activities)
+print "What are we going to do: " + the_activity
+
